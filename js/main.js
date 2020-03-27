cee6408c258d9e382a1a87170745cde8dd3a360e
ozzaynetworks <ozzaynetworks@gmail.com>
2020-03-27 10:38:20 +0300

    First files

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(window).on('load', function(){
+	
+//	custom day time picker
+	$('#daily_date').on('change', function(){
+		var dated=$(this).val();
+		var dated=(moment(dated).format('LL'));
+		$('.the_day').text(dated);
+//		alert(moment(dated).format('LL'));
+	});
+	
+	
+//	$('.table-ranger').on('change', function(){
+//		var therange=$(this).val();
+//		alert(therange);
+//	});
+	
+	$('.table-ranger').on('change', function(){
+		
+	});
+//	increase wigdth when value changes
+	function Expand(obj){
+      if (!obj.savesize) obj.savesize=obj.size;
+      obj.size=Math.max(obj.savesize,obj.value.length);
+     }
+	
+});
